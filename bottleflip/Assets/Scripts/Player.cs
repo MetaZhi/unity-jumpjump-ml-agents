88aa8eb3c80a2ee15e89e7d2e22b3c13711b5b0e
Manic <zhz@hongliu.cc>
2018-01-16 15:34:13 +0800

    restart and spawn stage

--- bottleflip/Assets/Scripts/Player.cs
+++ bottleflip/Assets/Scripts/Player.cs
@@ -1,18 +1,26 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public Transform Camera;
+    public GameObject Stage;
     public float Factor = 200;
     private Rigidbody _rigidbody;
     private float _startTime;
     private int _score;
+
+    private Vector3 _distanceToCamera;
+    private GameObject _lastStage;
 
     // Use this for initialization
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _rigidbody.centerOfMass = new Vector3(0, 0.01f, 0);
+        _distanceToCamera = Camera.position - transform.position;
+        _lastStage = Stage;
     }
 
     // Update is called once per frame
@@ -47,16 +55,26 @@ public class Player : MonoBehaviour
         {
             _score++;
             SpawnNextStage();
+            MoveCamera();
         }
+    }
+
+    private void MoveCamera()
+    {
+        var nextPosition = transform.position + _distanceToCamera;
+        Camera.DOMove(nextPosition, 1);
     }
 
     private void SpawnNextStage()
     {
-        throw new System.NotImplementedException();
+        var stage = Instantiate(Stage);
+        stage.transform.position = _lastStage.transform.position + new Vector3(Random.Range(1.1f,5),0,0);
+        _lastStage = stage;
     }
 
     private void Restart()
     {
-        
+        Debug.Log("restart");
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
     }
 }
